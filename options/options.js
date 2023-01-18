bc6264b580578a07312b6b25845722d0354c95ec
Crowdbotics <engineering@crowdbotics.com>
2023-01-18 17:22:55 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "test-komatsu-kpidas-57786",
+  url: "https://test-komatsu-kpidas-57786.botics.co",
+  api: "https://test-komatsu-kpidas-57786.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
